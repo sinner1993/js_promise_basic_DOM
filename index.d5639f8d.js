0a94f91623ebf4d218b49c2526cb368379f21b7c
sinner1993 <sergio1993goga@gmail.com>
2024-09-15 10:53:40 +0200

    make build

---
+++ index.d5639f8d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".logo"),t=new Promise(function(t){e.addEventListener("click",function(){t("Promise was resolved!")})}),n=new Promise(function(e,t){setTimeout(function(){t("Promise was rejected!")},3e3)});t.then(function(e){var t=document.querySelector("body"),n=document.createElement("div");n.setAttribute("class","message"),n.textContent=e,t.append(n)}),n.catch(function(e){var t=document.querySelector("body"),n=document.createElement("div");n.setAttribute("class","message error-message"),n.textContent=e,t.append(n)});
+//# sourceMappingURL=index.d5639f8d.js.map
